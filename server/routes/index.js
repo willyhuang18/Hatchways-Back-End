8fe49dc2e17d76158bc2ccf8adf55e8e8a700b06
willy <willyhuang18@gmail.com>
2022-04-11 21:32:02 -0700

    adding Check the input validations

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,18 @@ const Tags = (req, res) => {
     const api = `http://hatchways.io/api/assessment/blog/posts?tag=${tags}&sortBy=${sortBy}&direction=${direction}`;
     const sortFilter = ['id', 'likes', 'popularity', 'reads'];
     const directionFilter = ['asc', 'desc'];
-
+  
+    // Check the input validations
+    if (sortFilter.indexOf(sortBy) === - 1) {
+      res.status(400).send({
+        error: 'sortBy parameter is invalid',
+      });
+    }
+    if (directionFilter.indexOf(direction) === -1) {
+      res.status(400).send({
+        error: 'direction parameter is invalid',
+      });
+    }
+  
   }
   
